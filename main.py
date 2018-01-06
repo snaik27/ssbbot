56e86dce1509918e3106cdde39bffd3ba2466af9
Sid Naik <sid.naik27@gmail.com>
2018-01-05 17:57:11 -0800

    Works with updated dependencies
    Praw and bs4 updates broke the main.py script, also fixed character_data reply formatting and made code easier to understand

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from tabulate import tabulate
 
 #constants
 no_info ="Sorry I wasn't able to find this in my database, but I will update it as soon as possible!"
-signature = "This bot quickly provides information on Super Smash Bros lingo, characters, and events. Please pm with suggestions or problems at /u/sidwasnthere. To define Smash terms, type **ssb_bot define [smash_term]**. For frame data, type **ssb_bot [charactername]** followed by one of the following: **ground_moves/ aerials/ specials/ or properties**. For more information type **ssb_bot info**."
+signature = "This bot quickly provides information on Super Smash Bros lingo, characters, and events. Please pm with suggestions or problems at /u/sidwasnthere. To define Smash terms, type **ssb_bot define [smash_term]**. For frame data, type **ssb_bot [charactername]** followed by one of the following: **ground_moves, aerials, specials, or properties**. For more information type **ssb_bot info**."
 
 character_list=['Bayonetta','Bowser','Bowser Jr','Captain Falcon','Charizard','Cloud','Corrin',
                 'Dark Pit','Diddy Kong','Donkey Kong','Dr. Mario','Duck Hunt','Falco','Fox','Ganondorf',
@@ -27,67 +27,69 @@ character_list=['Bayonetta','Bowser','Bowser Jr','Captain Falcon','Charizard','C
 #functions:
 
 #Reply to user requests for character data
-def charactername(comment,function,no_info,signature,replied_to):
-        character= pd.read_csv('%s.csv'%function[1].capitalize(), encoding='utf-8')
-        if (function[1].capitalize() == 'Cloud'): 
+def charactername(comment,user_function,no_info,signature,replied_to):
+        character= pd.read_csv('%s.csv'%user_function[1].capitalize(), encoding='utf-8')
+        if (user_function[1].capitalize() == 'Cloud'): 
                 comment.reply('#OP' +'\n  ------------ \n' +signature)
-        elif (function[1].capitalize() == 'Bayonetta'):
+        elif (user_function[1].capitalize() == 'Bayonetta'):
                 comment.reply('#(╯ಠ_ಠ）╯︵ ┻━┻' + '\n  ------------ \n' +signature)
         else:
                 #Create Kurogane Hammer Link
                 kurogane = 'For a glossary of column definitions, please refer to the [Kurogane Hammer Glossary](http://kuroganehammer.com/Glossary)'
                 #Reply based on type of character data requested
                 #Each class of character data corresponds to one of the tables in Kurogane's website, all NA's are dropped, the table is then prettified, and comment is replied to
-                if (function[2]=='properties'):
+                if (user_function[2]=='properties'):
                         properties = character.iloc[:,:4].copy()
-                        properties.dropna(inplace=True)
-                        pretty = tabulate(properties, headers='keys',tablefmt="pipe")
-                        comment.reply('Smash 4 ' + fuction[1] + ' ' + function[2]+': \n' + pretty + '\n\n' + kurogane + '\n  ------------ \n' + signature)
+                        prettyreply(comment,user_function,properties,kurogane,signature)
                         
-                elif(function[2]=='ground_moves'):
+                elif(user_function[2]=='ground_moves'):
                         ground_moves=character.iloc[:,4:10].copy()
-                        ground_moves.dropna(inplace=True)
-                        pretty =tabulate(ground_moves,headers='keys',tablefmt="pipe")
-                        comment.reply('Smash 4 ' + fuction[1] + ' ' + function[2]+': \n' + pretty + '\n\n' + kurogane + '\n  ------------ \n' + signature)
+                        prettyreply(comment,user_function,ground_moves,kurogane,signature)
                         
-                elif(function[2]=='aerials'):
+                elif(user_function[2]=='aerials'):
                         aerials = character.iloc[:,11:20].copy()
-                        aerials.dropna(inplace=True)
-                        pretty =tabulate(aerials,headers='keys',tablefmt="pipe")
-                        comment.reply('Smash 4 ' + fuction[1] + ' ' + function[2]+': \n' + pretty + '\n\n' + kurogane + '\n  ------------ \n' + signature)
+                        prettyreply(comment,user_function,aerials,kurogane,signature)
                         
-                elif(function[2]=='specials'):
+                elif(user_function[2]=='specials'):
                         specials=character.iloc[:,21:27].copy()
-                        specials.dropna(inplace=True)
-                        pretty = tabulate(specials,headers='keys',tablefmt="pipe")
-                        comment.reply('Smash 4 ' + fuction[1] + ' ' + function[2]+': \n' + pretty + '\n\n' + kurogane + '\n  ------------ \n' + signature)
+                        prettyreply(comment,user_function,specials,kurogane,signature)
+                        
                 else:
                     comment.reply(no_info)
                     replied_to[comment].append(no_info)
-
+        record(comment, replied_to)
+        
+def prettyreply(comment, user_function, data, kurogane, signature):
+        data = data
+        data.dropna(inplace=True)
+        pretty=tabulate(data,headers='keys',tablefmt='pipe')
+        comment.reply('Smash 4 ' + user_function[1] + ' ' + user_function[2]+': \n\n' + pretty + '\n\n' + kurogane + '\n  ------------ \n' + signature)
+        print('replied')
 
 #Reply to requests for lingo definition
-def define(comment,function,no_info,signature,replied_to, terms):
+def define(comment,user_function,no_info,signature,replied_to, terms):
 
     #Format user inputs to work with our data
-    function=function[2:]
-    for word in range(0,len(function)):
-        function[word]=re.sub('[\W\s\_]','',function[word])
-        function[word]=re.sub('(ing|ly|ed|ious|ies|ive|es|s|ment)?$','',function[word])
-    function= ''.join(function)
-    function=function.lower()
+    user_function=user_function[2:]
+    for word in range(0,len(user_function)):
+        user_function[word]=re.sub('[\W\s\_]','',user_function[word])
+        user_function[word]=re.sub('(ing|ly|ed|ious|ies|ive|es|s|ment)?$','',user_function[word])
+    user_function= ''.join(user_function)
+    user_function=user_function.lower()
 
 
-    if function in terms:
-        term_url = requests.get(terms[function])
-        soup = BeautifulSoup(term_url.text,'lxml')
+    if user_function in terms:
+        term_url = requests.get(terms[user_function])
+        soup = BeautifulSoup(term_url.text,'html.parser')
         soup_stuff = soup.find('div',{'id':'mw-content-text'})
         soup_stuff = soup_stuff.find('p').get_text()
         comment.reply(soup_stuff +'\n  ------------ \n' + signature)
-        replied_to[comment].append(str(soup_stuff)
+        replied_to[comment].append(str(soup_stuff))
+        print('replied')
     else:
         comment.reply(no_info +'\n  ------------ \n' + signature)
         replied_to[comment].append(no_info)
+        print('replied')
 
     record(comment, replied_to)
 
@@ -96,7 +98,7 @@ def give_info(comment):
         more_info = 'We have over 200k subs on this subreddit, not to mention all the people that see top smash posts. So to all the smashers and spectators, new or old, please feel free to ask me for information.'
         and_more = 'This is a community effort! Thank you to all the contributors at Ssbwiki and of course KuroganeHammer. To see any of the information I provide and more, visit [The Ssbwiki](https://www.ssbwiki.com/) and [Kurogane Hammer](http://kuroganehammer.com/). Also thank you to the mods for allowing me to exist on this awesome sub and hopefully I can be of help around here.'
         comment.reply(signature +'\n  ------------ \n' + more_info + '\n\n' + and_more)
-
+        print('replied')
 
 #Record comments and replies
 def record(comment,replied_to):
@@ -115,7 +117,7 @@ with open('terms.csv', 'r',newline='', encoding='utf-8') as f:
 #create subreddit instance
 reddit = praw.Reddit('ssb_bot')
 
-smashbros= reddit.subreddit('smashbros')
+smashbros= reddit.subreddit('test')
 
 #Main script
 def main():
@@ -128,7 +130,7 @@ def main():
             if not (curr_comment.body is None) and not (curr_comment.author is None):
                 if curr_comment.body.lower().startswith('ssb_bot '):
                     #We want to make sure we haven't yet replied to the current comment. If we have, we skip the addition of the comment
-                    if curr_comment.replies.len() > 0:
+                    if curr_comment.replies.list():
                         i_list = set()
                         for i in curr_comment.replies.list():
                             i_list.add(str(i.author))
@@ -160,6 +162,7 @@ while restart == True:
         while True:
             restart = False
             main()
+            print('going to bed for now')
             time.sleep(120)
     except (RuntimeError,Exception,requests.exceptions.RequestException) as e:
         print(e)
